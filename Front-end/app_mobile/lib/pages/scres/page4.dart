a10886d0a2117cc8d201980d1965b982e44f8981
OthmaneBK <okebennani@gmail.com>
2024-03-31 14:36:58 +0000

    change

---
+++ Front-end/app_mobile/lib/pages/scres/page4.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Page4 extends StatelessWidget {
+  const Page4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+        child: Text("ok, it's good"),
+      ),
+    );
+  }
+}
